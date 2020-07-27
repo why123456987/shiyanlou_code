a6ebec8b085b366df2037b5b41fe365605ebf10e
yuanxing <2655049611@qq.com>
2020-07-27 16:30:04 +0800

    jump7 note

---
+++ jump7.py
@@ -0,0 +1,13 @@
+i = 1
+while i < 100:
+   if i % 7 == 0:
+       pass
+   elif i // 10 == 7:
+       pass
+   elif i % 10 == 7:
+       i += 1
+       continue
+   else:
+       print(i,end=" ")
+   i += 1
+
